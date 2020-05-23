16b06e9ed1e118f06720bee356f3b82c9933735a
Ratoriku <a1063021545@gmail.com>
2020-05-23 20:02:33 +0800

    nvt_touch_f7a: Mark performance critical
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.c
+++ drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.c
@@ -1362,9 +1362,9 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 		NVT_LOG("int_trigger_type=%d\n", ts->int_trigger_type);
 
 #if WAKEUP_GESTURE
-       		ret = request_threaded_irq(client->irq, NULL, nvt_ts_irq_handler, ts->int_trigger_type | IRQF_ONESHOT | IRQF_NO_SUSPEND, client->name, ts);
+       		ret = request_threaded_irq(client->irq, NULL, nvt_ts_irq_handler, ts->int_trigger_type | IRQF_ONESHOT | IRQF_PERF_CRITICAL |IRQF_NO_SUSPEND, client->name, ts);
 #else
-     		ret = request_threaded_irq(client->irq, NULL, nvt_ts_irq_handler, ts->int_trigger_type | IRQF_ONESHOT, client->name, ts);
+     		ret = request_threaded_irq(client->irq, NULL, nvt_ts_irq_handler, ts->int_trigger_type | IRQF_PERF_CRITICAL | IRQF_ONESHOT, client->name, ts);
 #endif
 		if (ret != 0) {
 			NVT_ERR("request irq failed. ret=%d\n", ret);
